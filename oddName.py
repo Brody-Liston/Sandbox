1c03e5eba727357b528f9a2e0a1c39a19a6566c2
Brody <brodyliston@gmail.com>
2018-03-06 14:39:37 +1300

    Add all of oddname program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,9 @@
 """Brody Liston"""
+min_lenght = 1
+
+user_name = str(input("Enter Name Please "))
+while len(user_name) < min_lenght:
+    print("Invalid Name Must not Be Blank")
+    user_name = str(input("Enter Name Please "))
+
+print((user_name)[::2])
